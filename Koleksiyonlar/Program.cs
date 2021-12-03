9372669204f210d7af1a91c98a089eebc408de31
Fabulous-levi <59694529+Fabulous-levi@users.noreply.github.com>
2021-12-03 11:05:50 +0300

    3/12/2021

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
